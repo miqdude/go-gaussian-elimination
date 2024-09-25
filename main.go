0b5896f8f8af806bf0b5fec7a050713b2698dea4
miqdude <enginrstudio@gmail.com>
2024-09-25 19:47:32 +0700

    fix formatted output

--- main.go
+++ main.go
@@ -14,11 +14,15 @@ func SolveLinearEquation(A [][]float64, b []float64) ([]float64, error) {
 	// Gaussian Elimination
 	rowA := len(A)
 	for i := 0; i < rowA-1; i++ {
+		// fmt.Printf("Step : %d\n", i)
 		for j := i + 1; j < rowA; j++ {
 			if A[i][i] == 0 {
 				continue
 			}
 			scalingFactor := A[j][i] / A[i][i]
+			fmt.Printf("STEP : %d\n", i+j)
+			fmt.Printf("Eliminating L%d with scaling factor from element %d,%d divided by element %d,%d\n", j+1, j+1, i+1, i+1, i+1)
+			fmt.Printf("L%d = L%d - (%.1f*L%d)\n\n", j+1, j+1, scalingFactor, i+1)
 
 			// reduce the current row with the row below
 			colA := rowA
@@ -30,14 +34,14 @@ func SolveLinearEquation(A [][]float64, b []float64) ([]float64, error) {
 			// do the same thing for matrix B
 			temp_ := b[j] - scalingFactor*b[i]
 			b[j] = temp_
-		}
 
-		// display per step
-		fmt.Printf("Step : %d\n", i)
-		showFormatted(A, b)
+			formattedPrint(A, b)
+			fmt.Print("\n")
+		}
 	}
 
 	// backward substitution
+	fmt.Print("Doing backward substitution\n\n")
 	res := make([]float64, rowA)
 	for i := rowA - 1; i >= 0; i-- {
 		if A[i][i] == 0 {
@@ -50,6 +54,7 @@ func SolveLinearEquation(A [][]float64, b []float64) ([]float64, error) {
 		}
 
 		res[i] = (b[i] - quantifier) / A[i][i]
+		fmt.Printf("%s = (%.1f - %.1f)/%.1f = %.1f\n\n", idxVar[i], b[i], quantifier, A[i][i], res[i])
 	}
 
 	return res, nil
@@ -87,63 +92,59 @@ func isRound(val float64) bool {
 	return mod == 0
 }
 
-func showFormatted(A [][]float64, b []float64) {
-	row := len(A)
-	for i := 0; i < row; i++ {
-		isLeading := true
-		for j := 0; j < row; j++ {
-			el := A[i][j]
-			if el == 0 {
-				continue
-			}
-
-			if !isLeading {
-				if el > 1 {
-					if isRound(el) {
-						fmt.Printf("+%.0f", el)
-					} else {
-						fmt.Printf("+%.1f", el)
-					}
-				} else if el < 1 {
-					if isRound(el) {
-						fmt.Printf("%.0f", el)
-					} else {
-						fmt.Printf("%.1f", el)
-					}
-				}
-			} else {
-				if isRound(el) {
-					fmt.Printf("%.0f", el)
-				} else {
-					fmt.Printf("%.1f", el)
-				}
-			}
-
-			isLeading = false
-			fmt.Print(idxVar[j], " ")
-		}
-
-		fmt.Printf("= %.0f\n", b[i])
+func formattedPrint(A [][]float64, b []float64) {
+	for i := 0; i < len(A); i++ {
+		showFormatted(A[i][:], b[i])
 	}
 }
 
-func printSolution(b []float64) {
-	fmt.Print("Solution: ")
-	for i := 0; i < len(b); i++ {
-		fmt.Printf("%s= %.1f ", idxVar[i], b[i])
+func showFormatted(A []float64, b float64) {
+	res := ""
+	isLeading := true
+	for i := 0; i < len(A); i++ {
+		if A[i] == 0 {
+			continue
+		}
+
+		if !isLeading && A[i] > 0 {
+			res += "+"
+		}
+
+		if A[i] != 1 && A[i] != -1 {
+			if isRound(A[i]) {
+				res += fmt.Sprintf("%.0f", A[i])
+			} else {
+				res += fmt.Sprintf("%.1f", A[i])
+			}
+		}
+
+		if A[i] == -1 {
+			res += "-"
+		}
+
+		res += idxVar[i]
+		isLeading = false
 	}
-	fmt.Println()
+
+	if isRound(b) {
+		res += fmt.Sprintf("=%.0f", b)
+	} else {
+		res += fmt.Sprintf("=%.1f", b)
+	}
+	fmt.Print(res, "\n")
 }
 
 func main() {
 	A, b := readInput()
 
+	fmt.Print("Solving : \n\n")
+	formattedPrint(A, b)
+	fmt.Println()
+
 	// Solve Ax = b
-	solution, err := SolveLinearEquation(A, b)
+	_, err := SolveLinearEquation(A, b)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
-	printSolution(solution)
 }
